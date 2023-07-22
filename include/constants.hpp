dd1b3a65a2b356da9720b60e1ee220b8725c7734
Joshua Jerred <jdjerred@gmail.com>
2023-07-22 06:28:24 -0500

    bitrate fix

--- include/constants.hpp
+++ include/constants.hpp
@@ -13,7 +13,7 @@
 
 namespace wavgen {
 
-inline constexpr uint32_t kSampleRate = 480000;
+inline constexpr uint32_t kSampleRate = 48000;
 inline constexpr uint32_t kBitsPerSample = 16;
 inline constexpr int32_t kMaxAmplitude = (1 << (kBitsPerSample - 1)) - 10;
 inline constexpr uint16_t kHeaderSize = 44;
